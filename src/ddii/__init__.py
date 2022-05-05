9b45f3deb34480f8c8905b7d0574fe8ad2a292f8
Luke Garzia <lgarzia@yahoo.com>
2022-05-05 07:22:24 -0500

    additional notes

--- src/ddii/__init__.py
+++ src/ddii/__init__.py
@@ -2,7 +2,12 @@ __version__ = '.1'
 
 print(f"In top level import statement for ddii")
 
+def function_top_level_one():
+    print('I am function from top level before call to subpackage')
+    
 from ddii.subpackage1.subpackage1b import hello_nested_1b #hello_world
-hello_nested_1b()
+#hello_nested_1b()
+def function_top_level_two():
+    print('I am function from top level after call to subpackage')
 
-print(f"In top level after all import")
+print(f"Top Level Import Statement Completed")
--- src/ddii/subpackage1/subpackage1b/__init__.py
+++ src/ddii/subpackage1/subpackage1b/__init__.py
@@ -1,4 +1,9 @@
 print("in nested subpackage of 1 in b")
+import ddii
+print("in nested subpackage of 1 in b - after top level import ddii call")
+
+ddii.function_top_level_one()
+ddii.function_top_level_two()
 
 def hello_nested_1b():
     print('hello world from nested 1b')
